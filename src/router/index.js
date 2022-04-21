181b6d9a6b170eb0e4ea9f816a5e850de5b85c7b
AJ <ajabad.dev@gmail.com>
2022-04-21 10:31:23 +0800

    Added router guard logic

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,28 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-import Home from "../views/Home";
+import SignIn from "../views/SignIn";
+import SignUp from "../views/SignUp";
 
 Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "SignIn",
+    component: SignIn,
+    meta: {
+      title: "Sign in",
+      requiresGuest: true,
+    },
+  },
+  {
+    path: "/signup",
+    name: "SignUp",
+    component: SignUp,
+    meta: {
+      title: "Create an account",
+      requiresGuest: true,
+    },
   },
 ];
 
@@ -16,6 +30,17 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+    ? `${to.meta.title} - ${process.env.VUE_APP_NAME}`
+    : process.env.VUE_APP_NAME;
+  if (to.meta.requiresGuest && !!localStorage.getItem("token"))
+    return next("/home");
+  if (to.meta.requiresAuth && !localStorage.getItem("token"))
+    return next("/");
+  return next();
 });
 
 export default router;
